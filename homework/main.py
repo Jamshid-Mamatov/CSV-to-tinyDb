230beb10186acadd86d7c393109bf5f6820a5457
Jamshid <mamatovjamshid506@gmail.com>
2021-02-18 20:34:13 +0500

    csv to tinydb

--- homework/main.py
+++ homework/main.py
@@ -4,12 +4,20 @@ db=TinyDB("data.json")
 
 def insert_db(data:str)->list:
     
+    data_list=data.split("\n")
+    key=data_list[0].split(",")[1:]
+
+    database=[]
+    for row in data_list[1:]:
+        doc_={}
+        for ind,item in enumerate(row.split(",")[1:]):
+            doc_[key[ind]]=item
+        
+        database.append(doc_)
+
     
-    
-    
-    
-    
-    return 0
+
+    return database
 
 
 
@@ -19,3 +27,5 @@ table2=db.table("specification")
 data1=open("products.csv").read()
 data2=open("specifications.csv").read()
 
+table1.insert_multiple(insert_db(data1))
+table2.insert_multiple(insert_db(data2))
