4f32836fc8b4bf2d444efd77b63a8ecf290cffd3
Jamshid <mamatovjamshid506@gmail.com>
2021-02-18 10:30:42 +0500

    csv to json

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-from tinydb import TinyDb
+from tinydb import TinyDB
+
+db = TinyDB('db.json')
+
+f=open("products.csv").read()
+# print(type(f))
+data=f.split("\n")[1:]
+# print(data)
+
+list_doc=[]
+for doc in data:
+    # print(doc)
+    doc_={}
+    cat,comp=doc.split(",")[1:]
+    
+
+    doc_['category']=cat
+    doc_['company']=comp
+    list_doc.append(doc_)
+    
+print(list_doc)
+
+
