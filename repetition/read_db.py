92c1ebd9c6c5a88d9a058806268f7bc8a263d361
Jamshid <mamatovjamshid506@gmail.com>
2021-02-25 12:52:04 +0500

    query ->search

--- repetition/read_db.py
+++ repetition/read_db.py
@@ -1,8 +1,12 @@
-from tinydb import TinyDB
+from tinydb import TinyDB,Query
 
 db=TinyDB("database.json")
 
+q=Query()
 data=db.all()
+user=db.search(q.username=="Jamshid")
 
-for i in data:
-    print(i)
+print(user)
+
+# for i in data:
+#     print(i)
